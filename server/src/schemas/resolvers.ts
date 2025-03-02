6a6c0c80f1004d72590c3ad5906355c20731a1dc
erin foley <erindagfoley@gmail.com>
2025-03-02 10:59:16 -0500

    added jwt for user

---
+++ server/src/schemas/resolvers.ts
@@ -0,0 +1,12 @@
+// import { BookDocument } from "../models/Book";
+// import Book { BookDocument } from '..models/Book.ts';
+// import Userimport { getSingleUser } from "../controllers/user-controller";
+//  { UserDocument } from '..models/User.ts';
+
+// const resolvers = {
+//     Query:
+//         getSingleUser: [User]
+//         login
+//     },
+// },
+//     Mutations: 
