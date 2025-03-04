ffb9d5d3c43508e99448ac35f9afa6070fe926a6
erin foley <erindagfoley@gmail.com>
2025-03-04 16:38:24 -0500

    fixed signup component

--- client/src/utils/mutations.ts
+++ client/src/utils/mutations.ts
@@ -13,9 +13,9 @@ export const LOGIN = gql`
   }
 `;
 
-export const CREATE_USER = gql`
-  mutation createUser($username: String!, $email: String!, $password: String!) {
-    createUser(username: $username, email: $email, password: $password) {
+export const ADD_USER = gql`
+  mutation addUser($username: String!, $email: String!, $password: String!) {
+    addUser(username: $username, email: $email, password: $password) {
       token
       user {
         _id
